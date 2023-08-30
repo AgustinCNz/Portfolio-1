3f8a077d42ed0bfd783e5ee74870255f818eff25
agustin <agustin_correanz@outlook.com>
2023-08-30 03:34:57 -0300

    Creacion del proyecto portfolio-1, Agregamos un archivo top-section.tsx en donde ingresamos una Image que deseamos mostrar con una breve descripcion, agregamos una carpeta assets en donde colocamos la Image anterior y agregamos un archivo top-section.txs en donde ingresamos texto para completar esta seccion

---
+++ src/app/description-section.tsx
@@ -0,0 +1,19 @@
+export default function DescriptionSection () {
+    return (
+        <div className="mb-12>">
+        <p className="text-gray-200">
+            Hi!, i'm a Software 
+            <span className="text-sky-500 font-bold">Developer</span>
+        </p>
+        <p className="text-gray-200">
+            I develop huge applications.
+        </p>
+        <p className="text-gray-200">
+            I code day and night.
+        </p>
+        <p className="text-gray-200">
+            I like coffe and minimal techno.
+        </p>
+        </div>
+    )
+}
